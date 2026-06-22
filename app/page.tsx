2660143779e7864588a0dd6a60451a6858c06537
Automatizaciones-GestorAI <gestorai.automatizaciones@gmail.com>
2026-06-22 23:50:15 +0200

    feat: add noise background to page

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,12 @@ import { PistasScroll } from '@/components/PistasScroll'
 import { HorariosSection } from '@/components/HorariosSection'
 import { CTASection } from '@/components/CTASection'
 import { Footer } from '@/components/Footer'
+import { NoiseBackground } from '@/components/NoiseBackground'
+
 export default function Home() {
   return (
     <main>
+      <NoiseBackground />
       <Navbar />
       <HeroSection />
       <PistasScroll />
