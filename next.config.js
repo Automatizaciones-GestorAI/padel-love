6d520a290d0aebf74a5bdc431dd861c7f0b93c63
Automatizaciones-GestorAI <gestorai.automatizaciones@gmail.com>
2026-06-23 21:29:44 +0200

    fix: remove invalid qualities option from next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    qualities: [100],
     formats: ['image/webp'],
     minimumCacheTTL: 31536000,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
 }
 module.exports = nextConfig
