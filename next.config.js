8ee59afd7ec8c9ed97695070c101e8121013c075
Automatizaciones-GestorAI <gestorai.automatizaciones@gmail.com>
2026-06-23 21:35:19 +0200

    feat: allow unsplash remote images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
     formats: ['image/webp'],
     minimumCacheTTL: 31536000,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
