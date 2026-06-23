11ced622d3717f4b582143e03f68b02833d8c655
Automatizaciones-GestorAI <gestorai.automatizaciones@gmail.com>
2026-06-23 21:25:51 +0200

    fix: max image quality config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: false,
+    qualities: [100],
+    formats: ['image/webp'],
+    minimumCacheTTL: 31536000,
   },
 }
 module.exports = nextConfig
