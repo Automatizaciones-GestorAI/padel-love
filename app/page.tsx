1b42d18160455f1b8fb5ad1903021cbd68024ee5
Automatizaciones-GestorAI <gestorai.automatizaciones@gmail.com>
2026-06-23 21:37:54 +0200

    feat: add all sections to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
+import { ImpactStats } from '@/components/ImpactStats'
+import { FindMatch } from '@/components/FindMatch'
+import { Tournaments } from '@/components/Tournaments'
+import { School } from '@/components/School'
+import { Ranking } from '@/components/Ranking'
+import { Facilities } from '@/components/Facilities'
+import { Contact } from '@/components/Contact'
 import { Footer } from '@/components/Footer'
 
 export default function Home() {
@@ -7,6 +14,13 @@ export default function Home() {
     <main>
       <Header />
       <Hero />
+      <ImpactStats />
+      <FindMatch />
+      <Tournaments />
+      <School />
+      <Ranking />
+      <Facilities />
+      <Contact />
       <Footer />
     </main>
   )
